10579a72851d6da04593be4e67d57936a3c44e82
Darshan <drshnptl137@gmail.com>
2025-01-27 16:29:24 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Heebo, Bebas_Neue } from "next/font/google";
+import { Heebo } from "next/font/google";
 
 const heebo = Heebo({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Leven Plus",
+  title: "Opsi Vault",
   description: "Your Partner for Empowering Tech Solutions",
 };
 
@@ -20,9 +20,9 @@ export default function RootLayout({
       <head>
         <meta
           name="keywords"
-          content="Leven Plus, Leven+, Leven +, Leven Plus Infotech, web development, app development, hosting services, AI solutions, IT consultancy, machine learning, cyber security, cloud solutions, vps, IT management"
+          content="Opsi Vault, Leven+, Leven +, Opsi Vault Infotech, web development, app development, hosting services, AI solutions, IT consultancy, machine learning, cyber security, cloud solutions, vps, IT management"
         />
-        <meta name="author" content="Leven Plus" />
+        <meta name="author" content="Opsi Vault" />
         <meta
           name="google-site-verification"
           content="I4bSKo4qhYXtLwP4_cCzA2AHadn6WgKE0KEOis6Wz5o"
